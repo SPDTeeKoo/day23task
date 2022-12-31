d1bf66e0e0466162381391e3ea9dffc84e67b56a
SPDTeeKoo <soumyadutta150@gmail.com>
2022-12-31 11:31:28 +0530

    1st_commit

---
+++ src/components/Users.js
@@ -0,0 +1,19 @@
+const Users = [
+  {
+    id: '1',
+    Name: 'Lionel Messi',
+    Age: '34',
+  },
+  {
+    id: '2',
+    Name: 'Cristiano Ronaldo',
+    Age: '36',
+  },
+  {
+    id: '3',
+    Name: 'Andres Iniesta',
+    Age: '40',
+  },
+];
+
+export default Users;
